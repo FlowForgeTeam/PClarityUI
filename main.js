51efd6959a99475b3f9ae84edbd13061d3cda485
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2025-06-11 16:41:17 +0300

    extended project structure; created first working window

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('hello!');
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        // width: 1920,
+        // height: 1080
+        width: 1280,
+        height: 720
+    });
+
+    win.loadFile('./renderer/index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
