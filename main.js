1a0636446553a413c65d9680d766ed128dc17f2b
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2025-06-11 18:48:50 +0300

    Set up default and minimum window size; made window remember its position and size even after app is closed; made UI app quit when all windows are closed

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 const { app, BrowserWindow } = require('electron');
+const windowStateKeeper = require('electron-window-state');
 
 const createWindow = () => {
+
+    let mainWindowState = windowStateKeeper({
+        defaultWidth: 1440,
+        defaultHeight: 960,
+    });
+    
     const win = new BrowserWindow({
-        // width: 1920,
-        // height: 1080
-        width: 1280,
-        height: 720
+        x: mainWindowState.x,
+        y: mainWindowState.y,
+        width: mainWindowState.width,
+        height: mainWindowState.height,
+        minWidth: 1280,
+        minHeight: 800,
+        show: false
     });
 
+    mainWindowState.manage(win);
+
     win.loadFile('./renderer/index.html');
+    win.show();
 }
 
 app.whenReady().then(() => {
     createWindow();
-});
+})
+
+app.on('window-all-closed', () => {
+    app.quit();
+})
