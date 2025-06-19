86ded7e95010cd23c44971cde3fea98872916aea
Andrii Negrub <zurg0505@gmail.com>
2025-06-19 14:52:43 +0300

    UI: Added 18 themes, fixed color representation

--- renderer/config/themes_config.js
+++ renderer/config/themes_config.js
@@ -1,19 +1,79 @@
 const themesConfig = [
+    // Dark Themes
     {
         name: 'default',
+        theme_tag: 'Rich Purple'
     },
     {
-        name: 'dark-purple',
-    },
-    {
-        name: 'light-red',
+        name: 'grey-purple',
+        theme_tag: 'Cyberpunk Neon'
     },
     {
         name: 'black-pink',
+        theme_tag: 'Black Pink'
     },
     {
-        name: 'vintage-pink',
-    }
-]
+        name: 'grey-pink',
+        theme_tag: 'Vintage Pink'
+    },
+    {
+        name: 'dark-blue-cyan',
+        theme_tag: 'Midnight Ocean'
+    },
+    {
+        name: 'dark-green',
+        theme_tag: 'Forest Night'
+    },
+    {
+        name: 'black-neon-green',
+        theme_tag: 'Terminal Green'
+    },
+    {
+        name: 'black-orange',
+        theme_tag: 'Volcanic Ash'
+    },
+    {
+        name: 'dark-red',
+        theme_tag: 'Black Roses'
+    },
+    
+    // Light Themes
+    {
+        name: 'light-purple',
+        theme_tag: 'Lavender Dream'
+    },
+    {
+        name: 'light-pink',
+        theme_tag: 'Sakura Light'
+    },
+    {
+        name: 'light-indigo',
+        theme_tag: 'Sky High'
+    },
+    {
+        name: 'white-blue',
+        theme_tag: 'Cloud Nine'
+    },
+    {
+        name: 'light-teal',
+        theme_tag: 'Tropical Lagoon'
+    },
+    {
+        name: 'light-green',
+        theme_tag: 'Mint Fresh'
+    },
+    {
+        name: 'light-yellow',
+        theme_tag: 'Lemon Zest'
+    },
+    {
+        name: 'light-orange',
+        theme_tag: 'Sunset Glow'
+    },
+    {
+        name: 'light-red',
+        theme_tag: 'Rose Garden'
+    },
+];
 
 module.exports = { themesConfig };
